0e27772f7c4e6f8920399538e875a6cf768c2c25
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-06-16 00:34:11 -0400

    docker-compose deployment

--- desktop-service/server.js
+++ desktop-service/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '2999';
 
 app.get('/info', (req, res) => res.status(200).json({
     status: 'UP',
