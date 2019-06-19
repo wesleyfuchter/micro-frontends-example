dc506f794f0e97e5644b47d4393e46d3f6db2c45
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-06-18 21:54:31 -0400

    include react deployment

---
+++ people-service/people-service-webapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {PeopleList} from "./PeopleList";
+
+function App() {
+  return (
+    <div className="App">
+      <PeopleList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ people-service/people-service-webapp/src/PeopleList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function PeopleList() {
+
+    return (
+      <h1>Hello PeopleList</h1>
+    );
+
+}
