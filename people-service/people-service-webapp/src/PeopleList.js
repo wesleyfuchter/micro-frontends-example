765c3a16d227b003f86b9de81d1f5e29da74a5a2
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-06-18 22:38:53 -0400

    add people list request to random user api

--- people-service/people-service-webapp/src/PeopleList.js
+++ people-service/people-service-webapp/src/PeopleList.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export function PeopleList() {
 
+    const [people, setPeople] = useState([]);
+
+    const handleFindAllPeople = () => {
+        findAllPeople()
+            .then(data => setPeople(data.results))
+            .catch(err => console.error(err));
+    };
+
+    const findAllPeople = async () => {
+        const headers = new Headers({
+            "Content-Type": "application/json",
+        });
+        const request = {
+            method: 'GET',
+            headers: headers,
+            mode: 'cors',
+            cache: 'default',
+        };
+        return await fetch('https://randomuser.me/api/?results=100', request)
+            .then(response => response.json());
+    };
+
+    useEffect(() => handleFindAllPeople(), []);
+
     return (
-      <h1>Hello PeopleList</h1>
+      <div>
+          <ul>
+              {people.map(row => <li key={row.email}>{row.email}</li>)}
+          </ul>
+      </div>
     );
 
 }
